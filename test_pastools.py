3fa35cab110249e507f9cf3ac5d8852190071a2b
Sho Hirose <sho.hirose@gmail.com>
2021-08-18 01:17:17 +0900

    Add a test for add_access_env_vars()

--- test_pastools.py
+++ test_pastools.py
@@ -2,20 +2,22 @@ import pastools
 import pytest
 import os
 
+
 def test_parse_url():
-    url = r'pbscp://host1/home/user1/test1.sh'
+    url = 'pbscp://host1/home/user1/test1.sh'
     host, path = pastools.parse_url(url)
     assert host == 'host1'
     assert path == '/home/user1/test1.sh'
 
 
 def test_parse_export_env():
-    env = r'HOME=/home/user1,MYVAR=1,PAS_PRIMARY_FILE=pbscp://host1/home/user1/test1.sh'
+    env = 'HOME=/home/user1,MYVAR=1,PAS_PRIMARY_FILE=pbscp://host1/home/user1/test1.sh'
     env_vars = pastools.parse_export_env(env)
     assert env_vars == {
         "HOME": "/home/user1",
         "MYVAR": "1",
         "PAS_PRIMARY_FILE": "pbscp://host1/home/user1/test1.sh"}
+
 
 def test_create_access_env_vars():
     host = 'host1'
@@ -25,6 +27,16 @@ def test_create_access_env_vars():
         'ACCESS_INPUT_FILES': 'test1.sh@host1:/home/user1/test1.sh',
         'ACCESS_OUTPUT_FILES': '*@host1:/home/user1',
         'ACCESS_RUNNING_FILES': '/home/user1'}
+
+
+def test_add_access_env_vars():
+    env = 'HOME=/home/user1,MYVAR=1,PAS_PRIMARY_FILE=pbscp://host1/home/user1/test1.sh'
+    new_env = pastools.add_access_env_vars(env)
+    assert new_env == 'HOME=/home/user1,MYVAR=1,PAS_PRIMARY_FILE=pbscp://host1/home/user1/test1.sh' + \
+        ',ACCESS_INPUT_FILES=test1.sh@host1:/home/user1/test1.sh' + \
+        ',ACCESS_OUTPUT_FILES=*@host1:/home/user1' + \
+        ',ACCESS_RUNNING_FILES=/home/user1'
+
 
 def test_create_logs():
     dirname = os.path.abspath('.')
